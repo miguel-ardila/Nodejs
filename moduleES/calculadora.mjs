92ef6fe3a59fa73e033d61184a48740c995fb9e5
Miguel Ardila <miguel_sxe@hotmail.com>
2023-09-12 10:32:43 -0500

    funciones con colores de chalk

--- moduleES/calculadora.mjs
+++ moduleES/calculadora.mjs
@@ -1,8 +1,15 @@
-
+import chalk from "chalk"
 //const { suma, rest, mult, divi } = require("./operaciones") CommonJS
 import { suma, rest, mult, divi } from "./operaciones.mjs"; // module ES
 
-console.log(suma(12, 8));
-console.log(rest(23, 16));
-console.log(mult(9, 3));
-console.log(divi(1, 5));
+/* console.log(chalk.blue(suma(12, 8)));
+console.log(chalk.red(rest(23, 16)));
+console.log(chalk.greenBright.bgGrey(mult(9, 3)));
+console.log(divi(1, 5)); */
+
+const resultado = rest(23, 16);
+if (resultado < 5){
+    console.log(chalk.red("resultado de la resta: " + resultado));
+} else {
+    console.log(chalk.green("resultado de la resta: " + resultado));
+}
