db237868ea04a28925dc6c922216b11bd8394cec
Miguel Ardila <miguel_sxe@hotmail.com>
2023-09-12 11:02:16 -0500

    funcion resta con chalk en commonJS

--- modulo-commonJS/calculadora.js
+++ modulo-commonJS/calculadora.js
@@ -1,6 +1,14 @@
+const chalk = require("chalk")
 const { suma, rest, mult, divi } = require("./operaciones")
 
-console.log(suma(12, 8));
+/* console.log(chalk.blue(suma(12, 8)));
 console.log(rest(23, 16));
 console.log(mult(9, 3));
-console.log(divi(1, 5));
+console.log(divi(1, 5)); */
+
+const resultado = rest(23, 16);
+if (resultado < 5 ){
+    console.log(chalk.red(" el resultado de la resta es: " + resultado));
+} else {
+    console.log(chalk.green(" el resultado de la resta es: " + resultado));
+}
